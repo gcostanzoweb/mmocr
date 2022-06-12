00d5e84be4bf4bf3e7ddf62ffc58656a57ff1999
Gabriele Costanzo <g.costanzo.web@gmail.com>
2022-06-12 22:05:25 +0200

    Added SROIE config, part 1

---
+++ configs/_base_/det_datasets/sroie.py
@@ -0,0 +1,28 @@
+dataset_type = 'IcdarDataset'
+data_root = 'data/sroie'
+
+train = dict(
+    type=dataset_type,
+    ann_file=f'{data_root}/instances_training.json',
+    #loader=dict(
+    #    type='AnnFileLoader',
+    #    parser=dict(
+    #        type='LineJsonParser',
+    #        keys=['filename', 'text'])),
+    img_prefix=f'{data_root}/imgs/training',
+    pipeline=None)
+
+test = dict(
+    type=dataset_type,
+    ann_file=f'{data_root}/instances_test.json',
+    #loader=dict(
+    #    type='AnnFileLoader',
+    #    parser=dict(
+    #        type='LineJsonParser',
+    #        keys=['filename', 'text'])),
+    img_prefix=f'{data_root}/imgs/test',
+    pipeline=None)
+
+train_list = [train]
+
+test_list = [test]
